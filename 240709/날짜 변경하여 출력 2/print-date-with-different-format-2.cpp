bf3d0d72e7a4f8499f41a1106fce4de6057f60bd
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-09 16:18:57 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240709/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    char s;
+    cin >> a >> s >> b >> s >> c;
+
+    cout << c << "." << a << "." << b;
+    return 0;
+}
