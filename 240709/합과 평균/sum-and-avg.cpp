04e1f566ef9efe4535e84fcbbd9a83a6e88e95c7
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-09 16:46:36 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240709/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int sum = a + b;
+    double avg = (double)(a+b) / 2;
+    cout << sum << " " << avg;
+    return 0;
+}
