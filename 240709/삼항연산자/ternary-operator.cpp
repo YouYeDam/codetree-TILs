d1d451fc1d09fddfacf2ba5d7b2829806855017a
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-09 17:37:00 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240709/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    string b;
+
+    b = a == 100 ? "pass" : "failure";
+
+    cout << b;
+    return 0;
+}
