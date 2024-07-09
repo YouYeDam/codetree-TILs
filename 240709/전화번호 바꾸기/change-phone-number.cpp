355b6dfbba8e17a3e7e29534d8dea7100d75b87e
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-09 16:23:23 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240709/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    char x;
+
+    cin >> x >> x >> x >> x;
+    cin >> a >> x >> b;
+    cout << "010-" << b << "-" << a;    
+    return 0;
+}
