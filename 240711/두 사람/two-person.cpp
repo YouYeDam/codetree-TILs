2949e87c2c78698edca319c331bb81cca05d67be
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-11 14:25:23 +0900

    Title: 두 사람, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240711/두 사람/two-person.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int aa, ba;
+    char ag, bg;
+
+    cin >> aa >> ag >> ba >> bg;
+
+    if (aa >= 19 || ba >= 19) {
+        if (ag == 'M' || bg == 'W') {
+            cout << 1;
+        }
+        else {
+            cout << 0;
+        }
+    }
+    else {
+        cout << 0;
+    }
+    return 0;
+}
