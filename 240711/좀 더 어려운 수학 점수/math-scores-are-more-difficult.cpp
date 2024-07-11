ca6a564ff39f799f613ae246a20ab40367e21086
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-11 14:23:19 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int am, ae;
+    int bm, be;
+    cin >> am >> ae >> bm >> be;
+
+    if (am > bm) {
+        cout << "A";
+    }
+    else if (bm > am) {
+        cout << "B";
+    }
+    else {
+        if (ae > be) {
+            cout << "A";
+        }
+        else {
+            cout << "B";
+        }
+    }
+    return 0;
+}
