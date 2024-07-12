ada564a290a3eda276bd7e08e44c8bb68354988f
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-12 10:48:00 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240712/몫 출력하기/print-share.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cnt = 0;
+    while (true) {
+        int n;
+        cin >> n;
+        if (n % 2 == 0) {
+            cout << n / 2 << endl;
+            cnt++;
+        }
+        if (cnt == 3) {
+            break;
+        }
+    }
+    return 0;
+}
