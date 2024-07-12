e386da2dbbe3a02e8d3b0389ae31ff3be9cb93ca
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-12 10:53:49 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240712/3n + 1 수열/3n-plus-1-sequence.cpp
+++ 240712/3n + 1 수열/3n-plus-1-sequence.cpp
@@ -7,6 +7,9 @@ int main() {
     int cnt = 0;
 
     while (true) {
+        if (n == 1) {
+            break;
+        }
         if (n % 2 == 0) {
             n /= 2;
         }
@@ -15,9 +18,6 @@ int main() {
             n++;
         }
         cnt++;
-        if (n == 1) {
-            break;
-        }
     }
     cout << cnt;
     return 0;
