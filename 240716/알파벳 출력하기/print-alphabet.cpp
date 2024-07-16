719c2bfb44f502f750bc682d6f4a07b92e12a53d
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-16 10:04:34 +0900

    Title: 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240716/알파벳 출력하기/print-alphabet.cpp
+++ 240716/알파벳 출력하기/print-alphabet.cpp
@@ -5,11 +5,13 @@ int main() {
     int n;
     cin >> n;
     int alp = 65;
-
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
             cout << (char)alp;
             alp++;
+            if (alp == 91) {
+                alp = 65;
+            }
         }
         cout << endl;
     }
