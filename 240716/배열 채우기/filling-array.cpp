23840816667ea1d15c7a9346635d00eb0bb546cf
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-16 11:25:48 +0900

    Title: 배열 채우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240716/배열 채우기/filling-array.cpp
+++ 240716/배열 채우기/filling-array.cpp
@@ -4,15 +4,18 @@ using namespace std;
 int main() {
     int arr[10];
     int stop_num;
-
+    bool IsStop = false;
     for (int i = 0; i < 10; i++) {
         cin >> arr[i];
         if (arr[i] == 0) {
             stop_num = i;
+            IsStop = true;
             break;
         }
     }
-
+    if (IsStop) {
+        stop_num = 9;
+    }
     for (int i = stop_num - 1; i >= 0; i--) {
         cout << arr[i] << " ";
     }
