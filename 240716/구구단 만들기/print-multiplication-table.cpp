e1c30a87741cf267a70d0739a2f93a9a3025d314
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-16 09:28:41 +0900

    Title: 구구단 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240716/구구단 만들기/print-multiplication-table.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = 1; i <= 9; i++) {
+        for (int j = b; j >= a; j -= 2) {
+            cout << j << " * " << i << " = " << i * j;
+            if (j != a) { 
+                cout << " / ";
+            }
+            else {
+                cout << endl;
+            }
+        }
+    }
+    return 0;
+}
