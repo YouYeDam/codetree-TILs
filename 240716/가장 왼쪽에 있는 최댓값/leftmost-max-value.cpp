7abbd81046b398bced7d8ceacb83b6fa5bdaa688
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-16 16:17:33 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240716/가장 왼쪽에 있는 최댓값/leftmost-max-value.cpp
+++ 240716/가장 왼쪽에 있는 최댓값/leftmost-max-value.cpp
@@ -4,27 +4,27 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int arr[1001];
+    int arr[1000];
 
-    for (int i = 1; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
     int spot_i = n;
 
     while (true) {
         int max_val = 0;
-        int max_i = 1;
+        int max_i = 0;
 
-        for (int i = 1; i < spot_i; i++) {
+        for (int i = 0; i < spot_i; i++) {
             if (arr[i] > max_val) {
                 max_val = arr[i];
                 max_i = i;
             }
         }
 
-        cout << max_i << " ";
+        cout << max_i + 1 << " ";
         spot_i = max_i;
-        if (spot_i == 1) {
+        if (spot_i == 0) {
             break;
         }
     }
