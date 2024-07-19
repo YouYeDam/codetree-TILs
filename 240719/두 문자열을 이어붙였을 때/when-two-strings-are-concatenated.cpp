2146f072be1a20ee572cf11c23cad697a4e3024d
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-19 15:49:27 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    string ab = a + b;
+    string ba = b + a;
+
+    if (ab == ba) {
+        cout << "true";
+    }
+    else {
+        cout << "false";
+    }
+    return 0;
+}
