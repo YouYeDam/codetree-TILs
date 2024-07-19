633d3afee84e3ac89f16efddd5c74cd87f4a9c6c
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-19 11:31:31 +0900

    Title: 파스칼의 삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/파스칼의 삼각형/pascal's-triangle.cpp
+++ 240719/파스칼의 삼각형/pascal's-triangle.cpp
@@ -5,15 +5,16 @@ int main() {
     int n;
     cin >> n;
     int arr[15][15];
+    
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j <= i; j++) {
             arr[i][j] = 1;
         }
     }
-
+    
     for (int i = 1; i < n; i++) {
-        for (int j = 1; j <= i; j++) {
+        for (int j = 1; j <= i - 1; j++) {
             arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
         }
     }
