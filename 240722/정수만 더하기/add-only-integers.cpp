9f4569d608209a3ba010f85ff1551ecee9d6211c
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-22 10:52:56 +0900

    Title: 정수만 더하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240722/정수만 더하기/add-only-integers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    
+    int sum = 0;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            sum += str[i] - '0';
+        }
+    }
+    cout << sum;
+    return 0;
+}
