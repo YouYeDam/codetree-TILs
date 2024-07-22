20a38d42a2f277ca8a311d23458bcb5ec9639399
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-22 16:32:35 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240722/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int FindDivisior(int a, int b) {
+    int sum = 0;
+    for (int i = a; i <= b; i++) {
+        bool IsDivisior = false;
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                IsDivisior = true;
+                break;
+            }
+        }
+        if (IsDivisior == false) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    int sum = FindDivisior(a, b);
+    cout << sum;
+    return 0;
+}
