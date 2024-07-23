4af1d07d40a8ce58f35c3450ee747ce2b1d5f941
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-23 11:09:21 +0900

    Title: 두 정수에 대한 연산값, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240723/두 정수에 대한 연산값/operational-values-for-two-integers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void ChangeNumber(int *a, int *b) {
+    if (a > b) {
+        *a += 25;
+        *b *= 2;
+    }
+    else {
+        *a *= 2;
+        *b += 25;
+    }
+}
+int main() {
+    int a, b;
+    cin >> a >> b;
+    ChangeNumber(&a, &b);
+
+    cout << a << " " << b;
+    return 0;
+}
