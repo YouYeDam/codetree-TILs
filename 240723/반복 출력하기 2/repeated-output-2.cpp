13eaf4a5cc48a189e1ba5a37f8909f7dd7a8cea9
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-23 13:43:55 +0900

    Title: 반복 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/반복 출력하기 2/repeated-output-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void PrintString(int n) {
+    if (n == 0) {
+        return;
+    }
+    PrintString(n - 1);
+    cout << "HelloWorld" << endl;
+}
+int main() {
+    int n;
+    cin >> n;
+    PrintString(n);
+    return 0;
+}
