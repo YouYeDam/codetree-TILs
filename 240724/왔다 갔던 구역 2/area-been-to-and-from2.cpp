a02517b32adba23f6b6545bb044d103fadc500c5
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-07-24 17:44:45 +0900

    Title: 왔다 갔던 구역 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240724/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240724/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -7,26 +7,19 @@ int main() {
     int arr[2000] = {};
     int idx = 1000;
 
-    char pre_c = 'a';
-
     for (int i = 0; i < n; i++) {
         int x;
         char c;
         cin >> x >> c;
 
-        if (pre_c != c) {
-            arr[idx]++;
-            pre_c = c;
-        }
-
         if (c == 'R') {
-            for (int j = idx + 1; j <= idx + x; j++) {
+            for (int j = idx; j <= idx + x; j++) {
                 arr[j]++;
             }
             idx += x;
         }
         else {
-            for (int j = idx - 1; j >= idx - x; j--) {
+            for (int j = idx; j >= idx - x; j--) {
                 arr[j]++;
             }
             idx -= x;
