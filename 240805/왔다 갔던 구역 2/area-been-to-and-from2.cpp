ce6130c0a34c0f5b707febecb7c149dc134dfcce
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-05 10:46:46 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240805/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240805/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -1,66 +1,33 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int n;
     cin >> n;
     int arr[2001] = {};
     int pos = 1000;
-    char prev_d = 'N';
-
-
     for (int i = 0; i < n; i++) {
         int x;
         char d;
         cin >> x >> d;
-
-        if (d == prev_d) {
-            if (d == 'R') {
-                for (int j = pos + 1; j <= pos + x; j++) {
-                    arr[j]++;
-                }
-            pos += x;
+        if (d == 'L') {
+            for (int j = pos-1; j >= pos-x; j--) {
+                arr[j]++;
             }
-
-            else {
-                for (int j = pos - 1; j >= pos - x; j--) {
-                    arr[j]++;
-                }
-                pos -= x;
-            }
+            pos -= x;
         }
         else {
-            if (d == 'R') {
-                for (int j = pos; j <= pos + x; j++) {
-                    arr[j]++;
-                }
-                pos += x;
+            for (int j = pos; j < pos+x; j++) {
+                arr[j]++;
             }
-            else {
-                for (int j = pos; j >= pos - x; j--) {
-                    arr[j]++;
-                }
-                pos -= x;
-            }
+            pos += x;
         }
-        prev_d = d;
     }
-
     int cnt = 0;
-    for (int i = 0; i < 2001; i++) {
+    for (int i = 0; i < 2000; i++) {
         if (arr[i] >= 2) {
-            for (int j = 1; j < 2001 - i; j++) {
-                if (arr[i+j] >= 2) {
-                    cnt++;
-                }
-                else {
-                    i += j;
-                    break;
-                }
-            }
+            cnt++;
         }
     }
-
     cout << cnt;
     return 0;
 }
