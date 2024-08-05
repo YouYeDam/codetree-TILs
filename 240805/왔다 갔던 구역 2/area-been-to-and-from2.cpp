3eda4b152093bdd9adcd33ae618a8b4f6ea67240
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-05 10:23:10 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240805/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240805/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -4,35 +4,63 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int arr[2000] = {};
-
+    int arr[2001] = {};
     int pos = 1000;
+    char prev_d = 'N';
+
+
     for (int i = 0; i < n; i++) {
         int x;
         char d;
-
         cin >> x >> d;
 
-        if (d == 'R') {
-            for (int j = pos; j < pos + x; j++) {
-                arr[j]++;
-            }
+        if (d == prev_d) {
+            if (d == 'R') {
+                for (int j = pos + 1; j <= pos + x; j++) {
+                    arr[j]++;
+                }
             pos += x;
+            }
+
+            else {
+                for (int j = pos - 1; j >= pos - x; j--) {
+                    arr[j]++;
+                }
+                pos -= x;
+            }
         }
         else {
-            for (int j = pos; j > pos - x; j--) {
-                arr[j]++;
+            if (d == 'R') {
+                for (int j = pos; j <= pos + x; j++) {
+                    arr[j]++;
+                }
+                pos += x;
             }
-            pos -= x;
+            else {
+                for (int j = pos; j >= pos - x; j--) {
+                    arr[j]++;
+                }
+                pos -= x;
+            }
         }
+        prev_d = d;
     }
+
     int cnt = 0;
-    
-    for (int i = 0; i < 2000; i++) {
+    for (int i = 0; i < 2001; i++) {
         if (arr[i] >= 2) {
-            cnt++;
+            for (int j = 1; j < 2001 - i; j++) {
+                if (arr[i+j] >= 2) {
+                    cnt++;
+                }
+                else {
+                    i += j;
+                    break;
+                }
+            }
         }
     }
+
     cout << cnt;
     return 0;
 }
