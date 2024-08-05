12408adf6175f2c40244338ca3ad360681e54d91
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-05 15:43:42 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240805/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
+++ 240805/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
@@ -9,20 +9,26 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
+    
 
     int max_val = 0;
     int cnt = 1;
+    bool IsExist = false;
 
     for (int i = 0; i < n; i++) {
         if (arr[i] > t && arr[i + 1] > arr[i]) {
             cnt++;
+            IsExist = true;
         }
         else {
-            if (arr[i] > t && cnt > max_val) {
+            if (cnt > max_val) {
                 max_val = cnt;
             }
             cnt = 1;
         }
+    }
+    if (IsExist == false) {
+        max_val = 0;
     }
     cout << max_val;
     return 0;
