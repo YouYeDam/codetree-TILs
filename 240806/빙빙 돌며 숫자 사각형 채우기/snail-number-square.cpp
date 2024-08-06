c1654856aeeb5c99537ff2e14651091955c45b67
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-06 12:27:00 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240806/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
+++ 240806/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -39,7 +39,7 @@ int main() {
     }
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < m; j++) {
             cout << arr[i][j] << " ";
         }
         cout << endl;
