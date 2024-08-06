1570c516b7f543fc9d183f39e7a233bf9e9d08b8
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-06 09:41:43 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240806/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
+++ 240806/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -2,40 +2,45 @@
 using namespace std;
 
 int main() {
-    int n, k, p, t;
-    cin >> n >> k >> p >> t;
+    int N, K, P, T;
+    cin >> N >> K >> P >> T;
     int infectee[101] = {};
     int infect_count[101] = {};
-    int infect_time[251][2] = {};
+    int infect_time[251][3] = {};
 
-    infectee[p] = 1;
+    infectee[P] = 1;
     int total_time = 0;
 
-    for (int i = 0; i < t; i++) {
-        int time, infectee_num, infected_num;
-        cin >> time >> infectee_num >> infected_num;
+    for (int i = 0; i < T; i++) {
+        int t, x, y;
+        cin >> t >> x >> y;
 
-        if (time > total_time) {
-            total_time = time;
+        if (t > total_time) {
+            total_time = t;
         }
 
-        infect_time[time][0] = infectee_num;
-        infect_time[time][1] = infected_num;
+        infect_time[t][0] = x; // 감염시킨자
+        infect_time[t][1] = y; // 감염당한자
+        infect_time[t][2] = 1; // 악수여부
     }    
 
     for (int i = 1; i <= total_time; i++) {
         int infectee_num = infect_time[i][0];
         int infected_num = infect_time[i][1];
+        
+        if (infect_time[i][2] != 1) { // 악수여부가 없으면 건너뛰기
+            continue;
+        }
 
         if (infectee[infectee_num] == 1) {
-            if (infect_count[infectee_num] <= k) {
+            if (infect_count[infectee_num] <= K) {
                 infectee[infected_num] = 1;
                 infect_count[infectee_num]++;
             }
         }
     }
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= N; i++) {
         cout << infectee[i];
     }
     return 0;
