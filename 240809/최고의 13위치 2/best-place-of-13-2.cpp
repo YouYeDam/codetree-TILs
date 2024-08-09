1761ace0b124366780bf544b98de45c64a8cafec
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-09 16:12:33 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240809/최고의 13위치 2/best-place-of-13-2.cpp
+++ 240809/최고의 13위치 2/best-place-of-13-2.cpp
@@ -5,7 +5,6 @@ int main() {
     int n;
     cin >> n;
     int arr[20][20] = {};
-
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> arr[i][j];
@@ -18,8 +17,16 @@ int main() {
         for (int j = 0; j < n - 2; j++) {
             for (int k = 0; k < n; k++) {
                 for (int v = 0; v < n - 2; v++) {
-                    if (k == i && v >= j && v <= j + 2) {
-                        continue;
+                    if (k == i) {
+                        if (v >= i && v <= i + 2) {
+                            continue;
+                        }
+                        if (v + 1 >= i && v + 1 <= i + 2) {
+                            continue;
+                        }
+                        if (v + 2 >= i && v + 2 <= i + 2) {
+                            continue;
+                        }
                     }
                     int sum1 = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
                     int sum2 = arr[k][v] + arr[k][v + 1] + arr[k][v + 2];
