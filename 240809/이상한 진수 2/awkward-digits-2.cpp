8df82bc00a90801bd00ea19a01ad4e7eb235bfc2
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-09 11:00:47 +0900

    Title: 이상한 진수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/이상한 진수 2/awkward-digits-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+    string max_val;
+    int res;
+    int len = a.length();
+
+    if (a[len - 1] == '0') {
+        for (int i = 0; i < a.length(); i++) {
+            if (a[i] == '0') {
+                a[i] = '1';
+                max_val = a;
+                break;
+            }
+        }
+    }
+    else {
+        a[len - 1] = '0';
+        max_val = a;
+    }
+    
+    int n = 0;
+    int prod = 1;
+    for (int i = max_val.length() - 1; i >= 0; i--) {
+        n += prod * (max_val[i] - '0');
+        prod *= 2;
+    }
+    cout << n;
+    return 0;
+}
