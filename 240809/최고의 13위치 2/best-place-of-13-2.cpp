6898a0b078f296870aafba2d686ef6ffffc3b1da
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-09 15:50:28 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/최고의 13위치 2/best-place-of-13-2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[20][20] = {};
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    int max_val = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 2; j++) {
+            for (int k = 0; k < n; k++) {
+                for (int v = 0; v < n - 2; v++) {
+                    if (k >= i && k <= i + 2 && v >= j && v <= j + 2) {
+                        continue;
+                    }
+                    int sum1 = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+                    int sum2 = arr[k][v] + arr[k][v + 1] + arr[k][v + 2];
+                    if (sum1 + sum2 > max_val) {
+                        max_val = sum1 + sum2;
+                    }
+                }
+            }
+        }
+    }
+    cout << max_val;
+    return 0;
+}
