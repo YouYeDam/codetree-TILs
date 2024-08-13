2726e3c70ef028f9ef7365340014b3a17466a184
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-13 13:40:33 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
+++ 240813/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
@@ -1,82 +1,123 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int n;
     cin >> n;
-
-    int arr_1[3][5];
-    int arr_2[3][5];
-
-
-    for (int i = 0; i < 3; i++) {
-        int num;
-        cin >> num;
-
-        int x = -2;
-        for (int j = 0; j < 5; j++) {
-            int res = num + x;
-            if (res < 1) {
-                res = n + x + num;
-            }
-            else if (res > n) {
-                if (num == n - 1) {
-                    res = 1;
-                }
-                else if (num == n) {
-                    res = 0 + x;
-                }
-            }
-
-            arr_1[i][j] = res;
-            x++;
-        }
-    }
+    int arr1[3];
+    int arr2[3];
+    int arr1_a[5];
+    int arr1_b[5];
+    int arr1_c[5];
+    int arr2_a[5];
+    int arr2_b[5];
+    int arr2_c[5];
 
     for (int i = 0; i < 3; i++) {
-        int num;
-        cin >> num;
-
-        int x = -2;
-        for (int j = 0; j < 5; j++) {
-            int res = num + x;
-            if (res < 1) {
-                res = n + x + num;
-            }
-            else if (res > n) {
-                if (num == n - 1) {
-                    res = 1;
-                }
-                else if (num == n) {
-                    res = 0 + x;
-                }
-            }
-
-            arr_2[i][j] = res;
-            x++;
-        }
+        cin >> arr1[i];
     }
-    int prod = 1;
-
     for (int i = 0; i < 3; i++) {
-        int arr_a[5];
-        int arr_b[5];
-        int sum = 0;
-        for (int j = 0; j < 5; j++) {
-            arr_a[j] = arr_1[i][j];
-            arr_b[j] = arr_2[i][j];
-        }
-
-        for (int j = 0; j < 5; j++) {
-            for (int k = 0; k < 5; k++) {
-                if (arr_a[j] == arr_b[k]) {
-                    sum++;
+        cin >> arr2[i];
+    }
+    if (n >= 5) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                int x = arr1[i];
+                if (x + j - 2 <= 0) {
+                    if (i == 0) {
+                        arr1_a[j] = x + j - 2 + n;
+                    }
+                    else if (i == 1) {
+                        arr1_b[j] = x + j - 2 + n;
+                    }
+                    else {
+                        arr1_c[j] = x + j - 2 + n;
+                    }
+                }
+                else if (x + j - 2 > n) {
+                    if (i == 0) {
+                        arr1_a[j] = x + j - 2 - n;
+                    }
+                    else if (i == 1) {
+                        arr1_b[j] = x + j - 2 - n;
+                    }
+                    else {
+                        arr1_c[j] = x + j - 2 - n;
+                    }
+                }
+                else {
+                    if (i == 0) {
+                        arr1_a[j] = x + j - 2;
+                    }
+                    else if (i == 1) {
+                        arr1_b[j] = x + j - 2;
+                    }
+                    else {
+                        arr1_c[j] = x + j - 2;
+                    }
                 }
             }
         }
-        prod *= sum;
-    }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                int x = arr2[i];
+                if (x + j - 2 <= 0) {
+                    if (i == 0) {
+                        arr2_a[j] = x + j - 2 + n;
+                    }
+                    else if (i == 1) {
+                        arr2_b[j] = x + j - 2 + n;
+                    }
+                    else {
+                        arr2_c[j] = x + j - 2 + n;
+                    }
+                }
+                else if (x + j - 2 > n) {
+                    if (i == 0) {
+                        arr2_a[j] = x + j - 2 - n;
+                    }
+                    else if (i == 1) {
+                        arr2_b[j] = x + j - 2 - n;
+                    }
+                    else {
+                        arr2_c[j] = x + j - 2 - n;
+                    }
+                }
+                else {
+                    if (i == 0) {
+                        arr2_a[j] = x + j - 2;
+                    }
+                    else if (i == 1) {
+                        arr2_b[j] = x + j - 2;
+                    }
+                    else {
+                        arr2_c[j] = x + j - 2;
+                    }
+                }
+            }
+        } 
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int cnt3 = 0;
 
-    cout << 250 - prod;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr1_a[i] == arr2_a[j]) {
+                    cnt1++;
+                }
+                if (arr1_b[i] == arr2_b[j]) {
+                    cnt2++;
+                }
+                if (arr1_c[i] == arr2_c[j]) {
+                    cnt3++;
+                }
+            }
+        }
+        int prod = cnt1 * cnt2 * cnt3;
+        int sum = 250 - prod;
+        cout << sum;
+    }
+    else {
+        cout << n * n * n;
+    }
     return 0;
 }
