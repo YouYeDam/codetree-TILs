49d79f5ce833809df07ca77ab59487e245689986
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-13 16:00:51 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240813/가장 가까운 두 점 사이의 거리/closest-pair-of-points.cpp
+++ 240813/가장 가까운 두 점 사이의 거리/closest-pair-of-points.cpp
@@ -11,26 +11,25 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i][0] >> arr[i][1];
     }
-
+    
     int min_dis = INT_MAX;
-    int res;
 
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             int x1 = arr[i][0];
             int y1 = arr[i][1];
+            
             int x2 = arr[j][0];
             int y2 = arr[j][1];
 
-            int dis = abs(x1 - x2) + abs(y1 - y2);
+            int dis = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
 
             if (dis < min_dis) {
                 min_dis = dis;
-                res = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
             }
         }
     }
 
-    cout << res;
+    cout << min_dis;
     return 0;
 }
