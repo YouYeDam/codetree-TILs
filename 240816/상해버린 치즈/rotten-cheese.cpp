805217cd3753692e6ef6a1d22d0a44f24aa0b656
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-16 13:52:30 +0900

    Title: 상해버린 치즈, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240816/상해버린 치즈/rotten-cheese.cpp
+++ 240816/상해버린 치즈/rotten-cheese.cpp
@@ -13,11 +13,13 @@ int main() {
         cin >> arr_info[i][0] >> arr_info[i][1] >> arr_info[i][2];
     }
 
+    arr_info[1][1] = 1; arr_info[1][2] = 3; 
+
     for (int i = 0; i < s; i++) {
         cin >> arr_ill[i][0] >> arr_ill[i][1];
     }
 
-    int arr_rot[50] = {};
+    int arr_rot[51][2] = {};
 
     for (int i = 0; i < s; i++) {
         for (int j = 0; j < d; j++) {
@@ -25,27 +27,36 @@ int main() {
                 continue;
             }
 
-            if (arr_info[j][2] <= arr_ill[i][1]) {
-                arr_rot[arr_info[j][1]]++;
+            if (arr_info[j][2] < arr_ill[i][1]) {
+                if (arr_rot[arr_info[j][1]][1] == 0) {
+                    arr_rot[arr_info[j][1]][0]++;
+                    arr_rot[arr_info[j][1]][1] = 1;
+                }
             }
+        }
+        for (int i = 1; i <= 50; i++) {
+            arr_rot[i][1] = 0;
         }
     }
 
     int max_val = 0;
 
     for (int i = 1; i <= 50; i++) {
-        if (arr_rot[i] != s) {
+        if (arr_rot[i][0] != s) {
             continue;
         }
         int val = 0;
+        int arr_eat[51][51] = {};
 
         for (int j = 0; j < d; j++) {
-            if (arr_info[j][1] == i) {
+            if (arr_info[j][1] == i && arr_eat[arr_info[j][0]][i] == 0) {
                 val++;
+                arr_eat[arr_info[j][0]][i] = 1;
             }
         }
         max_val = max(max_val, val);
     }
+
     cout << max_val;
 
     return 0;
