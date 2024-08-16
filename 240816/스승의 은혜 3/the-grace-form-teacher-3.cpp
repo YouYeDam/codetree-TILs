22a4c2bf36755534d47700a1809fd702afc85c80
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-16 11:34:09 +0900

    Title: 스승의 은혜 3, Time: 19ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240816/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 240816/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -5,55 +5,38 @@ using namespace std;
 
 int main() {
     int n, b;
-    int arr[1000][3];
-    int new_arr[1000][3];
     cin >> n >> b;
+    int arr[1000][2];
 
     for (int i = 0; i < n; i++) {
         cin >> arr[i][0] >> arr[i][1];
-        arr[i][2] = arr[i][0] + arr[i][1];
-    }
-    
-    int cnt = 0;
-    for (int i = 0; i < n; i++) {
-        int min_val = INT_MAX;
-        int min_num;
-
-        for (int j = 0; j < n; j++) {
-            if (arr[j][2] < min_val) {
-                min_val = arr[j][2];
-                min_num = j;
-            }
-        }
-
-        new_arr[cnt][0] = arr[min_num][0];
-        new_arr[cnt][1] = arr[min_num][1];
-        new_arr[cnt][2] = arr[min_num][2];
-        cnt++;
-
-        arr[min_num][2] = INT_MAX;
     }
 
     int max_num = 0;
 
     for (int i = 0; i < n; i++) {
-        int sum = 0;
-        int cnt = 0;
+        int new_arr[1000];
         for (int j = 0; j < n; j++) {
             if (i == j) {
-                sum += new_arr[j][0] / 2;
+                new_arr[j] = arr[j][0] / 2 + arr[j][1];
             }
             else {
-                sum += new_arr[j][0];
+                new_arr[j] = arr[j][0] + arr[j][1];
             }
-            sum += new_arr[j][1];
-            cnt++;
         }
-        if (sum > b) {
-            max_num = max(max_num, cnt - 1);
+
+        sort(new_arr, new_arr + n);
+
+        int sum = 0;
+
+        for (int j = 0; j < n; j++) {
+            sum += new_arr[j];
+            if (sum > b) {
+                max_num = max(max_num, n - 1);
+                break;
+            }
         }
     }
-
     cout << max_num;
     return 0;
 }
