9107b72ce9e80c458e8f36fe83fdd2d70390bb7c
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-20 17:44:45 +0900

    Title: 독서실의 거리두기 5, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240820/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    string str;
+    cin >> n >> str;
+    int max_diff = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        string new_str = str;
+        if (new_str[i] == '1') {
+            continue;
+        }
+
+        new_str[i] = '1';
+        int min_diff = INT_MAX;
+        int idx = 0;
+
+        for (int j = 1; j < n; j++) {
+            if (new_str[j] == '1') {
+                int diff = j - idx;
+                min_diff = min(min_diff, diff);
+                idx = j;
+            }
+        }
+        max_diff = max(max_diff, min_diff);
+    }
+    cout << max_diff;
+    return 0;
+}
