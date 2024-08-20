10b8a3dad86f14b12b7add190be0b8237251a57c
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-20 16:45:42 +0900

    Title: 원소 값들의 최대 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240820/원소 값들의 최대 합/maximum-sum-of-element-values.cpp
+++ 240820/원소 값들의 최대 합/maximum-sum-of-element-values.cpp
@@ -6,31 +6,27 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
-    int arr[101];
-
+    
+    int arr[101] = {};
     for (int i = 1; i <= n; i++) {
         cin >> arr[i];
     }
-
     int max_sum = 0;
 
     for (int i = 1; i <= n; i++) {
-        int temp_arr[101]; 
+        int temp_arr[101] = {}; 
 
         for (int j = 1; j <= n; j++) {
             temp_arr[j] = arr[j];
         }
-        int temp = temp_arr[i];
-        int idx = temp;
+
+        int idx = i;
         int sum = 0;
 
         for (int j = 0; j < m; j++) {
-            temp = temp_arr[idx];
-            temp_arr[idx] = idx;
-            sum += idx;
-            idx = temp;
+            sum += temp_arr[idx];
+            idx = temp_arr[idx];
         }
-
         max_sum = max(max_sum, sum);
     }
     cout << max_sum;
