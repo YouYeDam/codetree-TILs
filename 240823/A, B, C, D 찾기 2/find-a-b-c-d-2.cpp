6c67d6569fba8fa98ae73d42fe9be728b41eea3c
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-23 14:54:49 +0900

    Title: A, B, C, D 찾기 2, Time: 14ms, Memory: 0MB, Status: Passed - CodeTree

--- 240823/A, B, C, D 찾기 2/find-a-b-c-d-2.cpp
+++ 240823/A, B, C, D 찾기 2/find-a-b-c-d-2.cpp
@@ -10,17 +10,46 @@ int main() {
     }
     sort(arr, arr + 15);
 
-    int a = arr[0];
-    int abcd = arr[14];
-    int bcd = arr[13];
-    int acd = arr[12];
-    int abd = arr[11];
+    for (int i = 1; i <= 40; i++) {
+        for (int j = i; j <= 40; j++) {
+            for (int k = j; k <= 40; k++) {
+                for (int l = k; l <= 40; l++) {
+                    int a = i;
+                    int b = j;
+                    int c = k;
+                    int d = l;
 
-    int c = bcd - abd + a;
-    int d = acd - a - c;
-    int b = abcd - a - c - d;
+                    int ab = a + b;
+                    int bc = b + c;
+                    int cd = c + d;
+                    int da = d + a;
+                    int ac = a + c;
+                    int bd = b + d;
 
-    cout << a << " " << b << " " << c << " " << d;
+                    int abc = a + b + c;
+                    int abd = a + b + d;
+                    int acd = a + c + d;
+                    int bcd = b + c + d;
 
+                    int abcd = a + b + c + d;
+
+                    int new_arr[15] = {a, b, c, d, ab, bc, cd, da, ac, bd, abc, abd, acd, bcd, abcd};
+                    sort(new_arr, new_arr + 15);
+
+                    bool IsSame = true;
+
+                    for (int i = 0; i < 15; i++) {
+                        if (arr[i] != new_arr[i]) {
+                            IsSame = false;
+                        }
+                    }
+                    if (IsSame) {
+                        cout << a << " " << b << " " << c << " " << d;
+                        break;
+                    }
+                }
+            }
+        }
+    }
     return 0;
 }
