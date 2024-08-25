8a8dec2d20c06124b89628ea5d814fc33b681418
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-08-25 15:14:11 +0900

    Title: 언덕 깎기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240825/언덕 깎기/hill-cutting.cpp
+++ 240825/언덕 깎기/hill-cutting.cpp
@@ -12,12 +12,13 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-
     int total_val = 0;
     
     while (true) {
         int max_height = INT_MIN;
         int min_height = INT_MAX;
+        int max_cnt = 0;
+        int min_cnt = 0;
         int max_idx;
         int min_idx;
 
@@ -32,22 +33,47 @@ int main() {
             }
         }
 
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == arr[max_idx]) {
+                max_cnt++;
+            }
+            if (arr[i] == arr[min_idx]) {
+                min_cnt++;
+            }
+        }
+
         int diff = max_height - min_height;
         int max_height_value = 0;
         int min_height_value = 0;
 
         while (true) {
-            max_height--;
-            max_height_value++;
-            arr[max_idx]--;
-            if (max_height - min_height <= 17) {
-                break;
+            if (min_cnt >= max_cnt) {
+                max_height--;
+                max_height_value++;
+                arr[max_idx]--;
+                if (max_height - min_height <= 17) {
+                    break;
+                }
+                min_height++;
+                min_height_value++;
+                arr[min_idx]++;
+                if (max_height - min_height <= 17) {
+                    break;
+                }
             }
-            min_height++;
-            min_height_value++;
-            arr[min_idx]++;
-            if (max_height - min_height <= 17) {
-                break;
+            else {
+                min_height++;
+                min_height_value++;
+                arr[min_idx]++;
+                if (max_height - min_height <= 17) {
+                    break;
+                }
+                max_height--;
+                max_height_value++;
+                arr[max_idx]--;
+                if (max_height - min_height <= 17) {
+                    break;
+                }
             }
         }
 
@@ -60,7 +86,6 @@ int main() {
             check_max_height = max(check_max_height, arr[i]);
             check_min_height = min(check_min_height, arr[i]);
         }
-
         if (check_max_height - check_min_height <= 17) {
             break;
         }
