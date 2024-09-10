16f485cf804dff21f3ff07d60b2b982e0031c35f
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-09-10 10:35:32 +0900

    Title: 두 번째로 작은 수의 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240910/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
+++ 240910/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
 using namespace std;
 
 int main() {
@@ -11,20 +12,42 @@ int main() {
         cin >> arr[i];
     }
 
-    sort(arr, arr + n);
+    int min_val = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        min_val = min(min_val, arr[i]);
+    }
 
+    int sec_val = INT_MAX;
+    int idx;
     bool IsExist = false;
-    for (int i = 1; i < n; i++) {
-        if (arr[0] != arr[i]) {
-            if (arr[i] != arr[i+1]) {
-                IsExist = true;
-                cout << i + 1;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == min_val) {
+            continue;
+        }
+
+        if (arr[i] < sec_val) {
+            sec_val = arr[i];
+            idx = i;
+            IsExist = true;
+        }
+    }
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == sec_val) {
+            cnt++;
+            if (cnt >= 2) {
+                IsExist = false;
+                break;
             }
-            break;
         }
     }
 
-    if (!IsExist) {
+    if (IsExist) {
+        cout << idx + 1;
+    }
+    else {
         cout << -1;
     }
     return 0;
