23130859915de2ac04dfd287a348cd2047e8900e
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-09-10 11:01:09 +0900

    Title: L, R 그리고 B, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240910/L, R 그리고 B/l-r-and-b.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char arr[10][10];
+
+    int lx, ly, rx, ry, bx, by;
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cin >> arr[i][j];
+            if (arr[i][j] == 'B') {
+                bx = i;
+                by = j;
+            }
+            else if (arr[i][j] == 'R') {
+                rx = i;
+                ry = j;
+            }
+            else if (arr[i][j] == 'L') {
+                lx = i;
+                ly = j;
+            }
+        }
+    }
+
+    if (lx != bx && ly != by) {
+        int sum = abs(lx - bx) + abs(ly - by) - 1;
+        cout << sum;
+    }
+    else if (lx == bx) {
+        if ((ly < ry && ry < by) || (by < ry && ry < ly)) {
+            int sum = abs(ly - by) + 1;
+        }
+        else {
+            int sum = abs(ly - by) - 1;
+            cout << sum;
+        }
+    }
+    else if (ly == by) {
+        if ((lx < rx && rx < bx) || (bx < rx && rx < lx)) {
+            int sum = abs(lx - bx) + 1;
+        }
+        else {
+            int sum = abs(lx - bx) - 1;
+            cout << sum;
+        }
+    }
+    return 0;
+}
