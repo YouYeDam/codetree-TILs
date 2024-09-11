ac66eebfccc2750c3d60f73ec101f1e600ce539c
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-09-11 19:11:22 +0900

    Title: 최소 와이파이 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240911/최소 와이파이 수/minimum-number-of-wifi.cpp
+++ 240911/최소 와이파이 수/minimum-number-of-wifi.cpp
@@ -5,7 +5,7 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    int arr[100];
+    int arr[100] = {};
 
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
@@ -14,13 +14,13 @@ int main() {
     int idx = m;
     int cnt = 0;
     int x = m * 3 + 1;
-    
+
     while (idx < n + x) {
         for (int i = idx - m; i <= idx + m; i++) {
-
             if (i < 0 || i >= n) {
                 continue;
             }
+
             if (arr[i] == 1) {
                 cnt++;
                 break;
