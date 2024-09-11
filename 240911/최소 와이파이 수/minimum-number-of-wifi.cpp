cf214bb8cfe654c3b7635203003141d664a77c73
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-09-11 18:51:00 +0900

    Title: 최소 와이파이 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240911/최소 와이파이 수/minimum-number-of-wifi.cpp
+++ 240911/최소 와이파이 수/minimum-number-of-wifi.cpp
@@ -4,48 +4,29 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
+
     int arr[100];
+
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
+    int idx = m;
     int cnt = 0;
-    int wifi_idx = m;
-    for (int i = wifi_idx; i < n; i+= m*2+1) {
-        if (m == 0 || m >= n - 1) {
-            break;
-        }
-        for (int j = wifi_idx - m; j < wifi_idx + m; j++) {
-            if (j < 0 || j > n - 1) {
+    int x = m * 3 + 1;
+    
+    while (idx < n + x) {
+        for (int i = idx - m; i <= idx + m; i++) {
+
+            if (i < 0 || i >= n) {
                 continue;
             }
-            if (arr[j] == 1) {
+            if (arr[i] == 1) {
                 cnt++;
                 break;
             }
         }
-        if (i + m*2 + 1 > n) {
-            cnt++;
-        }
-    }
-    if (m == 0) {
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 1) {
-                cnt++;
-            }
-        }
-    }
-    else if (m >= n - 1) {
-        bool IsNeed = false;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 1) {
-                IsNeed = true;
-                cnt = 1;
-            }
-        }
-        if (!IsNeed) {
-            cnt = 0;
-        }
+        idx += x - m;
     }
     cout << cnt;
     return 0;
