50ca04eb1f322230809d3458cd03ff12734afd36
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-09-13 17:15:54 +0900

    Title: 수열의 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240913/수열의 순서 바꾸기/reorder-sequence.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[101];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    arr[n] = 101;
+
+    int cnt = 0;
+    while (true) {
+        int max_val = 0;
+        int max_val_idx;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max_val) {
+                max_val = arr[i];
+                max_val_idx = i;
+            }
+        }
+        int obj_idx = n - 1;
+        for (int i = max_val_idx + 1; i < n; i++) {
+            if (arr[i] > arr[0]) {
+                obj_idx = i - 1;
+            }
+        }
+        int temp = arr[0];
+        for (int i = 0; i < obj_idx; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[obj_idx] = temp;
+        cnt++;
+        bool IsSort = true;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i]> arr[i+1]) {
+                IsSort = false;
+                break;
+            }
+        }
+
+        if (IsSort) {
+            break;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
+
+//제일 큰 숫자 뒤 + 나보다 큰 숫자 나오기 전
