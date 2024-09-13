c55ce898b93aa535f6644034bafbe5aed282917e
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-09-13 17:29:35 +0900

    Title: 수열의 순서 바꾸기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240913/수열의 순서 바꾸기/reorder-sequence.cpp
+++ 240913/수열의 순서 바꾸기/reorder-sequence.cpp
@@ -37,7 +37,7 @@ int main() {
         int obj_idx = n - 1;
         bool IsBetween = false;
         for (int i = 1; i < n - 1; i++) {
-            if (arr[i] < arr[i] && arr[i + 1] > arr[i]) {
+            if (arr[i] < arr[0] && arr[i + 1] > arr[0]) {
                 obj_idx = i;
                 IsBetween = true;
             }
