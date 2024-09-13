455cf49ad7af5fbb1eac1f2d90fdeb27f64a9697
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-09-13 21:14:36 +0900

    Title: 수열의 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240913/수열의 순서 바꾸기/reorder-sequence.cpp
+++ 240913/수열의 순서 바꾸기/reorder-sequence.cpp
@@ -4,78 +4,53 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int arr[100] = {};
-    int min_val = 1000;
+    int arr[100];
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
-        min_val = min(min_val, arr[i]);
     }
 
     int cnt = 0;
-
     while (true) {
         bool IsSort = true;
         for (int i = 0; i < n - 1; i++) {
             if (arr[i] > arr[i + 1]) {
                 IsSort = false;
+                break;
             }
         }
         if (IsSort) {
             cout << cnt;
             break;
         }
-
-        int obj_idx;
-        int sec_idx;
-        int sec_val = 1000;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == min_val) {
-                continue;
-            }
-            if (arr[i] < sec_val) {
-                sec_val = arr[i];
-                sec_idx = i;
+        int last_idx = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[0]) {
+                last_idx = i;
             }
         }
 
-        bool IsSecsort = true;
-        for (int i = sec_idx; i < n - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                IsSecsort = false;
-            }
-        }
-
-        if (IsSecsort) {
-            obj_idx = sec_idx - 1;
-        }
-        else {
-            for (int i = sec_idx + 1; i < n; i++) {
-                bool IsAfterSort = true;
-                for (int j = i; j < n - 1; j++) {
-                    if (arr[j] > arr[j + 1]) {
-                        IsAfterSort = false;
-                    }
-                }
-                if (IsAfterSort) {
-                    obj_idx = i - 1;
+        int obj_idx = n - 1;
+        for (int i = last_idx + 1; i < n; i++) {
+            bool IsAfterSort = true;
+            for (int j = i; j < n - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    IsAfterSort = false;
                     break;
                 }
             }
-        }
-
-        for (int i = 1; i < n; i++) {
-            if (arr[0] > arr[i]) {
-                obj_idx = i;
+            if (IsAfterSort) {
+                obj_idx = i - 1;
+                break;
             }
         }
-
+        
         int temp = arr[0];
         for (int i = 0; i < obj_idx; i++) {
-            arr[i] = arr[i+1];
+            arr[i] = arr[i + 1];
         }
         arr[obj_idx] = temp;
-
         cnt++;
+
     }
     return 0;
 }
