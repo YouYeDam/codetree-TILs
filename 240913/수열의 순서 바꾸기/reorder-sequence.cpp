39e726f6bcf8f05ef34bd0fc3d1adf5257163710
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-09-13 20:21:08 +0900

    Title: 수열의 순서 바꾸기, Time: 1065ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240913/수열의 순서 바꾸기/reorder-sequence.cpp
+++ 240913/수열의 순서 바꾸기/reorder-sequence.cpp
@@ -5,10 +5,12 @@ int main() {
     int n;
     cin >> n;
     int arr[100] = {};
-
+    int min_val = 1000;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
+        min_val = min(min_val, arr[i]);
     }
+
     int cnt = 0;
 
     while (true) {
@@ -22,19 +24,47 @@ int main() {
             cout << cnt;
             break;
         }
-        int max_idx = 0;
+
+        int obj_idx;
+        int sec_idx;
+        int sec_val = 1000;
         for (int i = 0; i < n; i++) {
-            if (arr[i] > arr[max_idx]) {
-                max_idx = i;
+            if (arr[i] == min_val) {
+                continue;
+            }
+            if (arr[i] < sec_val) {
+                sec_val = arr[i];
+                sec_idx = i;
             }
         }
-        int obj_idx = max_idx;
+
+        bool IsSecsort = true;
+        for (int i = sec_idx; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                IsSecsort = false;
+            }
+        }
+        if (IsSecsort) {
+            obj_idx = sec_idx - 1;
+        }
+        else {
+            int min_idx;
+            int min_num = 1000;
+            for (int i = sec_idx + 1; i < n; i++) {
+                if (arr[i] < min_num) {
+                    min_num = arr[i];
+                    min_idx = i;
+                }
+            }
+            obj_idx = min_idx - 1;
+        }
 
         for (int i = 1; i < n; i++) {
             if (arr[0] > arr[i]) {
                 obj_idx = i;
             }
         }
+
         int temp = arr[0];
         for (int i = 0; i < obj_idx; i++) {
             arr[i] = arr[i+1];
@@ -44,8 +74,4 @@ int main() {
         cnt++;
     }
     return 0;
-
-    // 가장 마지막에 존재하는 얘보다 작은 숫자 뒤
-    // 작은 숫자가 없을 경우 가장 큰 숫자 뒤
-
 }
