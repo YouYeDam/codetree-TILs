d10bda231b9ce2a55ec5281a0b1690063bc02eae
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-09-13 19:26:02 +0900

    Title: 수열의 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240913/수열의 순서 바꾸기/reorder-sequence.cpp
+++ 240913/수열의 순서 바꾸기/reorder-sequence.cpp
@@ -4,60 +4,42 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int arr[101];
+    int arr[100] = {};
 
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    arr[n] = 101;
-
     int cnt = 0;
+
     while (true) {
         bool IsSort = true;
-
-        for (int i = 0; i < n; i++) {
-            if (arr[i]> arr[i+1]) {
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
                 IsSort = false;
-                break;
             }
         }
-
         if (IsSort) {
+            cout << cnt;
             break;
         }
+        int obj_idx = n - 2;
 
-        int max_val = 0;
-        int max_val_idx;
         for (int i = 1; i < n; i++) {
-            if (arr[i] > max_val) {
-                max_val = arr[i];
-                max_val_idx = i;
-            }
-        }
-        int obj_idx = n - 1;
-        bool IsBetween = false;
-        for (int i = 1; i < n - 1; i++) {
-            if (arr[i] < arr[0] && arr[i + 1] > arr[0]) {
+            if (arr[0] > arr[i]) {
                 obj_idx = i;
-                IsBetween = true;
-            }
-        }
-        if (!IsBetween) {
-            for (int i = max_val_idx + 1; i < n; i++) {
-                if (arr[i] > arr[0]) {
-                    obj_idx = i - 1;
-                }
             }
         }
         int temp = arr[0];
         for (int i = 0; i < obj_idx; i++) {
-            arr[i] = arr[i + 1];
+            arr[i] = arr[i+1];
         }
         arr[obj_idx] = temp;
+
         cnt++;
     }
-    cout << cnt;
     return 0;
-}
 
-//제일 큰 숫자 뒤 + 나보다 큰 숫자 나오기 전
+    // 가장 마지막에 존재하는 얘보다 작은 숫자 뒤
+    // 작은 숫자가 없을 경우 가장 마지막에 존재하는 얘보다 큰 숫자 앞 = 마지막 앞
+
+}
