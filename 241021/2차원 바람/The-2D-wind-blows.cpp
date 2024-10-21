2e90b1e1f9028896bf1c8fce0390833712c7b912
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-10-21 16:32:20 +0900

    Title: 2차원 바람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241021/2차원 바람/The-2D-wind-blows.cpp
+++ 241021/2차원 바람/The-2D-wind-blows.cpp
@@ -13,61 +13,63 @@ int main() {
             res_arr[i][j] = arr[i][j];
         }
     }
-
     
-    int r1, c1, r2, c2;
-    cin >> r1 >> c1 >> r2 >> c2;
+    for (int x = 0; x < q; x++) {
+        int r1, c1, r2, c2;
+        cin >> r1 >> c1 >> r2 >> c2;
 
-    r1--; c1--; r2--; c2--;
-    
-    int tmp1 = arr[r1][c2];
-    for (int i = c2; i > c1; i--) {
-        arr[r1][i] = arr[r1][i-1];
-    }
+        r1--; c1--; r2--; c2--;
+        
+        int tmp1 = arr[r1][c2];
+        for (int i = c2; i > c1; i--) {
+            arr[r1][i] = arr[r1][i-1];
+        }
 
-    int tmp2 = arr[r2][c2];
-    for (int i = r2; i > r1 + 1; i--) {
-        arr[i][c2] = arr[i-1][c2];
-    }
-    arr[r1 + 1][c2] = tmp1;
+        int tmp2 = arr[r2][c2];
+        for (int i = r2; i > r1 + 1; i--) {
+            arr[i][c2] = arr[i-1][c2];
+        }
+        arr[r1 + 1][c2] = tmp1;
 
-    int tmp3 = arr[r2][c1];
-    for (int i = c1; i < c2 - 1; i++) {
-        arr[r2][i] = arr[r2][i+1];
-    }
-    arr[r2][c2 - 1] = tmp2;
+        int tmp3 = arr[r2][c1];
+        for (int i = c1; i < c2 - 1; i++) {
+            arr[r2][i] = arr[r2][i+1];
+        }
+        arr[r2][c2 - 1] = tmp2;
 
-    for (int i = r1; i < r2 - 1; i++) {
-        arr[i][c1] = arr[i+1][c1];
-    }
-    arr[r2 - 1][c1] = tmp3;
+        for (int i = r1; i < r2 - 1; i++) {
+            arr[i][c1] = arr[i+1][c1];
+        }
+        arr[r2 - 1][c1] = tmp3;
 
-    for (int i = r1; i <= r2; i++) {
-        for (int j = c1; j <= c2; j++) {
-            int sum = arr[i][j];
-            int cnt = 1;
+        for (int i = r1; i <= r2; i++) {
+            for (int j = c1; j <= c2; j++) {
+                int sum = arr[i][j];
+                int cnt = 1;
 
-            if (i > 0) {
-                sum += arr[i - 1][j];
-                cnt++;
+                if (i > 0) {
+                    sum += arr[i - 1][j];
+                    cnt++;
+                }
+                if (i < n - 1) {
+                    sum += arr[i + 1][j];
+                    cnt++;
+                }
+                if (j > 0) {
+                    sum += arr[i][j - 1];
+                    cnt++;
+                }
+                if (j < m - 1) {
+                    sum += arr[i][j + 1];
+                    cnt++;
+                }
+
+                int avg = sum / cnt;
+                res_arr[i][j] = avg;
             }
-            if (i < n - 1) {
-                sum += arr[i + 1][j];
-                cnt++;
-            }
-            if (j > 0) {
-                sum += arr[i][j - 1];
-                cnt++;
-            }
-            if (j < m - 1) {
-                sum += arr[i][j + 1];
-                cnt++;
-            }
-
-            int avg = sum / cnt;
-            res_arr[i][j] = avg;
         }
     }
+
 
 
 
