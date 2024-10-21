515194673c5cd84d9bf88643f93ebaea255c65b5
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-10-21 17:26:19 +0900

    Title: 최단 Run Length 인코딩, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241021/최단 Run Length 인코딩/shortest-run-length-encoding.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int Encoding(string a) {
+    string str = "";
+
+    int cnt = 1;
+    char cur_char = a[0];
+
+    for (int i = 1; i < a.length(); i++) {
+        if (a[i] == cur_char) {
+            cnt++;
+        }
+        else {
+            str += cur_char + to_string(cnt);
+            cur_char = a[i];
+            cnt = 1;
+        }
+        if (i == a.length() - 1) {
+            str += cur_char + to_string(cnt);
+        }
+    }
+    return str.length();
+}
+
+int main() {
+    string a;
+    cin >> a;
+
+    
+    int n = a.length();
+    int min_len = Encoding(a);
+
+    for (int i = 0; i < n - 1; i++) {
+        int tmp = a[n - 1];
+
+        for (int j = n - 1; j > 0; j--) {
+            a[j] = a[j - 1];
+        }
+        a[0] = tmp;
+
+        int len = Encoding(a);
+        min_len = min(min_len, len);
+    }
+
+    cout << min_len;
+    
+    return 0;
+}
