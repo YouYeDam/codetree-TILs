1a52720641351a8cee74a3a7fe99a2cb0625293b
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-10-21 13:22:16 +0900

    Title: 양수 직사각형의 최대 크기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241021/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
+++ 241021/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -14,6 +14,7 @@ int main() {
             cin >> arr[i][j];
         }
     }
+
     
     for (int i = 0; i < n; i++) { // 가로행 기준
         for (int j = 0; j < m; j++) {
@@ -21,7 +22,6 @@ int main() {
                 continue;
             }
 
-            int size = 1;
             int col_size = 1;
 
             for (int k = j + 1; k < m; k++) {
@@ -32,73 +32,29 @@ int main() {
                     break;
                 }
             }
-            size = col_size;
             
-            if (size == 1) {
-                max_size = max(max_size, size);
+            if (col_size == 1) {
+                max_size = max(max_size, col_size);
                 break;
             }
 
-            for (int l = i + 1; l < n; l++) {
-                bool IsPositive = true;
-                for (int p = j; p < j + col_size; p++) {
-                    if (arr[l][p] < 1) {
-                        IsPositive = false;
+            for (int q = 1; q <= col_size; q++) {
+                int temp_size = q;
+                for (int l = i + 1; l < n; l++) {
+                    bool IsPositive = true;
+                    for (int p = j; p < j + q; p++) {
+                        if (arr[l][p] < 1) {
+                            IsPositive = false;
+                            break;
+                        }
+                    }
+                    if (!IsPositive) {
                         break;
                     }
+                    temp_size += q;
                 }
-                if (!IsPositive) {
-                    break;
-                }
-
-                size += col_size;
+                max_size = max(max_size, temp_size);
             }
-
-            max_size = max(max_size, size);
-        }
-    }
-
-
-    for (int i = 0; i < n; i++) { // 세로열 기준
-        for (int j = 0; j < m; j++) {
-            if (arr[i][j] < 1) {
-                continue;
-            }
-
-            int size = 1;
-            int row_size = 1;
-
-            for (int k = i + 1; k < n; k++) {
-                if (arr[k][j] >= 1) {
-                    row_size++;
-                }
-                else {
-                    break;
-                }
-            }
-            size = row_size;
-            
-            if (size == 1) {
-                max_size = max(max_size, size);
-                break;
-            }
-
-            for (int l = j + 1; l < m; l++) {
-                bool IsPositive = true;
-                for (int p = i; p < i + row_size; p++) {
-                    if (arr[p][l] < 1) {
-                        IsPositive = false;
-                        break;
-                    }
-                }
-                if (!IsPositive) {
-                    break;
-                }
-
-                size += row_size;
-            }
-
-            max_size = max(max_size, size);
         }
     }
 
