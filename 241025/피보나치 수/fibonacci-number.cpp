cbd2103c26c44138fce4838d6ddb0c0b93c603d1
YouYedam <99802899+YouYeDam@users.noreply.github.com>
2024-10-25 15:09:45 +0900

    Title: 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241025/피보나치 수/fibonacci-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[46] = {};
+
+int main() {
+    cin >> n;
+
+    arr[1] = 1;
+    arr[2] = 1;
+
+    for (int i = 3; i <= n; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    cout << arr[n];
+    return 0;
+}
